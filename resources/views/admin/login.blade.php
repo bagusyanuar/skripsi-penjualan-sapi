f3968e3333ffd7d26b901a2292037ca57cbd6cea
bagusyanuar <bagus.yanuar613@gmail.com>
2024-05-14 13:10:57 +0700

    form login admin

---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{csrf_token()}}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'/>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css"
+          rel="stylesheet"/>
+    <link href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css" rel="stylesheet"/>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet"/>
+    <link href="{{ asset('/css/style.admin.css') }}" rel="stylesheet"/>
+    <title>Form Login Admin | Heli Farm</title>
+</head>
+<body>
+<div class="login-container">
+    <div class="card-login-form">
+        <img src="{{ asset('/assets/image/logo.png') }}" alt="brand-image" class="bottom-space-medium">
+        <p class="font-normal color-dark fw-bold bottom-space-large">Form Login</p>
+        <form method="post" class="w-100">
+            @csrf
+            <div class="form-login">
+                <div class="w-100">
+                    <label for="username" class="form-label d-none"></label>
+                    <input type="text" placeholder="username" class="form-control custom-form-control w-100" id="username"
+                           name="username" aria-describedby="emailHelp">
+                </div>
+                <div class="w-100">
+                    <label for="password" class="form-label d-none"></label>
+                    <input type="password" placeholder="password" class="form-control custom-form-control"
+                           id="password" name="password" aria-describedby="emailHelp">
+                </div>
+                <div class="w-100 d-flex justify-content-end align-items-center">
+                    <button type="submit" class="btn-login">Login</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.7.0.js" type="text/javascript"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+</body>
+</html>
