43aebd6dcbca3414341a626325433a4965c89686
bagusyanuar <bagus.yanuar613@gmail.com>
2024-05-19 22:28:38 +0700

    product page

--- resources/views/member/product/index.blade.php
+++ resources/views/member/product/index.blade.php
@@ -1,7 +1,13 @@
 @extends('member.layout')
 
 @section('content')
-    <div class="w-100 d-flex justify-content-center mb-3">
-        <p class="section-title">Our Products</p>
+    <div class="w-100 d-flex justify-content-between align-items-center mb-3">
+        <p class="page-title">Products</p>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb mb-0">
+                <li class="breadcrumb-item"><a href="{{ route('member.home') }}">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Product</li>
+            </ol>
+        </nav>
     </div>
 @endsection
